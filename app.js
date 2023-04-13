2ce03317e067a500cf917a0ccead7105b16e16c2
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-04-13 09:17:39 +0100

    UPDATE: app.js

--- app.js
+++ app.js
@@ -33,25 +33,25 @@ function Product(name, src) {
 }
 
 // ********************************** Create products **********************************
-let bag = new Product('bag', './assets/images/bag.jpg');
-let banana = new Product('banana', './assets/images/banana.jpg');
-let bathroom = new Product('bathroom', './assets/images/bathroom.jpg');
-let boots = new Product('boots', './assets/images/boots.jpg');
-let breakfast = new Product('breakfast', './assets/images/breakfast.jpg');
-let bubblegum = new Product('bubblegum', './assets/images/bubblegum.jpg');
-let chair = new Product('chair', './assets/images/chair.jpg');
-let cthulhu = new Product('cthulhu', './assets/images/cthulhu.jpg');
-let dogDuck = new Product('dog-duck', './assets/images/dog-duck.jpg');
-let dragon = new Product('dragon', './assets/images/dragon.jpg');
-let pen = new Product('pen', './assets/images/pen.jpg');
-let petSweep = new Product('pet-sweep', './assets/images/pet-sweep.jpg');
-let scissors = new Product('scissors', './assets/images/scissors.jpg');
-let shark = new Product('shark', './assets/images/shark.jpg');
-let sweep = new Product('sweep', './assets/images/sweep.jpg');
-let tauntaun = new Product('tauntaun', './assets/images/tauntaun.jpg');
-let unicorn = new Product('unicorn', './assets/images/unicorn.jpg');
-let waterCan = new Product('water-can', './assets/images/water-can.jpg');
-let wineGlass = new Product('wine-glass', './assets/images/wine-glass.jpg');
+let bag = new Product('Bag', './assets/images/bag.jpg');
+let banana = new Product('Banana', './assets/images/banana.jpg');
+let bathroom = new Product('Bathroom', './assets/images/bathroom.jpg');
+let boots = new Product('Boots', './assets/images/boots.jpg');
+let breakfast = new Product('Breakfast', './assets/images/breakfast.jpg');
+let bubblegum = new Product('Bubblegum', './assets/images/bubblegum.jpg');
+let chair = new Product('Chair', './assets/images/chair.jpg');
+let cthulhu = new Product('Cthulhu', './assets/images/cthulhu.jpg');
+let dogDuck = new Product('Dog-duck', './assets/images/dog-duck.jpg');
+let dragon = new Product('Dragon', './assets/images/dragon.jpg');
+let pen = new Product('Pen', './assets/images/pen.jpg');
+let petSweep = new Product('Pet-sweep', './assets/images/pet-sweep.jpg');
+let scissors = new Product('Scissors', './assets/images/scissors.jpg');
+let shark = new Product('Shark', './assets/images/shark.jpg');
+let sweep = new Product('Sweep', './assets/images/sweep.jpg');
+let tauntaun = new Product('Tauntaun', './assets/images/tauntaun.jpg');
+let unicorn = new Product('Unicorn', './assets/images/unicorn.jpg');
+let waterCan = new Product('Water can', './assets/images/water-can.jpg');
+let wineGlass = new Product('Wine glass', './assets/images/wine-glass.jpg');
 
 state.products.push(
 	bag,
